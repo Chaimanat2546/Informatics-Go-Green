54045fd5aa1b956e68a3ac59b08e9112fe123547
Chaimanat2546 <152174623+Chaimanat2546@users.noreply.github.com>
2026-01-29 15:09:13 +0700

    feat: Add core TypeORM entities and module setup for waste management.

---
+++ backend/src/waste/entities/index.ts
@@ -0,0 +1,6 @@
+export { WasteCategory } from './waste-category.entity';
+export { Waste } from './waste.entity';
+export { WasteHistory } from './waste-history.entity';
+export { WasteSorting } from './waste-sorting.entity';
+export { WasteMaterial } from './waste-material.entity';
+export { MaterialGuide } from './material-guide.entity';
