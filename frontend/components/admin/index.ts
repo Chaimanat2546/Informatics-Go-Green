04de25ac666feccc0e00fa2685de6780a65fea72
Chaimanat Aepsuk <152174623+Chaimanat2546@users.noreply.github.com>
2026-02-06 12:38:47 +0700

    Revert "Develop"

--- frontend/components/admin/index.ts
+++
@@ -1,5 +0,0 @@
-// Admin Components Barrel Export
-export { default as AdminSidebar } from './AdminSidebar';
-export { default as AdminUsersTable } from './AdminUsersTable';
-export { default as AdminUserProfile } from './AdminUserProfile';
-export { default as ToggleUserStatusModal } from './ToggleUserStatusModal';
